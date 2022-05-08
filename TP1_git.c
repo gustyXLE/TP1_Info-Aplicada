bb41b0d2f9a627e48ea4a114da7ced0e285b56ca
Gustavo Garcia <gustyXLE>
2022-05-08 20:54:28 -0300

    Hice un breve codigo para empezar.

---
+++ TP1_git.c
@@ -0,0 +1,18 @@
+/*
+ * TP1.c
+ *
+ *  Created on: 8 may. 2022
+ *      Author: gusty
+ */
+
+/*
+ * Codigo TP1 de Info Aplicada.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+	printf("Bienvenido!!\n");
+	return 0;
+}
